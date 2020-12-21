2ae261e9891a8b486e55c8e6d331e1d0f4b7c798
lucas morales <wellucas151999@gmail.com>
2020-12-21 11:09:31 -0400

    First projetc Node API

---
+++ README.md
@@ -0,0 +1,2 @@
+# API-REST--api_petshop
+curso de nodejs for API REST 
---
+++ api/Erros/NaoEncontrado.js
@@ -0,0 +1,11 @@
+const { Module } = require("module")
+
+class NaoEncontrado extends Error {
+    constructor () {
+        super('Fornecedor não foi encontrado!')
+        this.name = 'NaoEncontrado'
+        this.idErro = 0
+    }
+}
+
+module.exports = NaoEncontrado
